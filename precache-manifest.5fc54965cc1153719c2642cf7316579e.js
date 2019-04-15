ffc3fea221ec504076292499925c80e0a144aa69
Sh1l0 <andrey.shilkin2010@yandex.ru>
2019-04-15 22:28:09 +0700

    build script is ready. Test2

---
+++ precache-manifest.5fc54965cc1153719c2642cf7316579e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc302acacdfee4bf2823",
+    "url": "/static/js/main.f3f1010c.chunk.js"
+  },
+  {
+    "revision": "809a99cd1c72dccbf989",
+    "url": "/static/js/2.99da2619.chunk.js"
+  },
+  {
+    "revision": "cc302acacdfee4bf2823",
+    "url": "/static/css/main.46218156.chunk.css"
+  },
+  {
+    "revision": "a68ec9f8acc2f69a5a9d1ecb544e9e5c",
+    "url": "/index.html"
+  }
+];
