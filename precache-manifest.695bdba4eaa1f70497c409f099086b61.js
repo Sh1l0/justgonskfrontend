a6dc9d710968c90a57f9b94690594d9b2690f937
Sh1l0 <andrey.shilkin2010@yandex.ru>
2019-04-21 12:11:45 +0700

    1.0.6.7 -ready for release. Fixed filters.

---
+++ precache-manifest.695bdba4eaa1f70497c409f099086b61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9b42f72d076360f60fd1",
+    "url": "/static/js/main.1e76e7bc.chunk.js"
+  },
+  {
+    "revision": "5f4f3c4c33bd10fec6ed",
+    "url": "/static/js/2.58f192ec.chunk.js"
+  },
+  {
+    "revision": "9b42f72d076360f60fd1",
+    "url": "/static/css/main.bcac308d.chunk.css"
+  },
+  {
+    "revision": "5746d9f32e5676e53896cc685ba63f60",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c90bd12c2b7026d42e4332811187a970.js"
+  "/precache-manifest.695bdba4eaa1f70497c409f099086b61.js"
 );
 
 workbox.clientsClaim();
