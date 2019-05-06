ab767ee036b79d80481150800911981e2cec4078
Sh1l0 <andrey.shilkin2010@yandex.ru>
2019-05-06 18:45:30 +0700

    0.4.1 -new style of map

--- src/components/Map.js
+++ src/components/Map.js
@@ -97,7 +97,7 @@ export default class MapPage extends Component {
         <TileLayer
           minZoom={12}
           maxZoom={19}
-          url="http://b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          url="http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           />
           <div className='map__control'>
