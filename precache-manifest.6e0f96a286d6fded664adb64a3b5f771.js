d3ff8992cd5107cea97871eff9c14b9c047562e2
Sh1l0 <andrey.shilkin2010@yandex.ru>
2019-05-29 00:23:45 +0700

    Updates

---
+++ precache-manifest.6e0f96a286d6fded664adb64a3b5f771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8753f3a471db6f39c032",
+    "url": "/justgonskfrontend/static/js/runtime~main.c2479a3a.js"
+  },
+  {
+    "revision": "a12430ab61c0e5b092dc",
+    "url": "/justgonskfrontend/static/js/main.351edc41.chunk.js"
+  },
+  {
+    "revision": "430fe7ff9298b25a3a0c",
+    "url": "/justgonskfrontend/static/js/2.d2e74668.chunk.js"
+  },
+  {
+    "revision": "a12430ab61c0e5b092dc",
+    "url": "/justgonskfrontend/static/css/main.6798c9a0.chunk.css"
+  },
+  {
+    "revision": "430fe7ff9298b25a3a0c",
+    "url": "/justgonskfrontend/static/css/2.542ffcac.chunk.css"
+  },
+  {
+    "revision": "e198aab101ccaa0d9e0c71577213ff6e",
+    "url": "/justgonskfrontend/index.html"
+  }
+];
