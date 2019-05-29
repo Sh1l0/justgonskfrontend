1438b91081169df5121251b636e8b885d257e7c5
Sh1l0 <andrey.shilkin2010@yandex.ru>
2019-05-29 22:53:25 +0700

    1.0.0 Beta Release

---
+++ build/precache-manifest.d28bfe115beed1e76f26b04a17c1571e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8753f3a471db6f39c032",
+    "url": "/justgonskfrontend/static/js/runtime~main.c2479a3a.js"
+  },
+  {
+    "revision": "59ae77640761a99f8606",
+    "url": "/justgonskfrontend/static/js/main.47bfff13.chunk.js"
+  },
+  {
+    "revision": "59df1c25aa297d2c657b",
+    "url": "/justgonskfrontend/static/js/2.3e8df433.chunk.js"
+  },
+  {
+    "revision": "59ae77640761a99f8606",
+    "url": "/justgonskfrontend/static/css/main.2a9dd440.chunk.css"
+  },
+  {
+    "revision": "59df1c25aa297d2c657b",
+    "url": "/justgonskfrontend/static/css/2.094287c3.chunk.css"
+  },
+  {
+    "revision": "5fc65b2218aa3596d5d6bfcfc4a1e80c",
+    "url": "/justgonskfrontend/index.html"
+  }
+];
