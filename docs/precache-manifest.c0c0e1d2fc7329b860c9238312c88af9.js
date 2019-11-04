d1a8b5310b8a53a4efeaf06db301405d792135c3
Sh1l0 <andrey.shilkin2010@yandex.ru>
2019-11-04 12:15:37 +0700

    added mocks

---
+++ docs/precache-manifest.c0c0e1d2fc7329b860c9238312c88af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb737662528cba24df35",
+    "url": "/static/css/main.b127d45d.chunk.css"
+  },
+  {
+    "revision": "eb737662528cba24df35",
+    "url": "/static/js/main.4a4e2478.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7f71e6275c384b3298a5",
+    "url": "/static/css/2.756c22b3.chunk.css"
+  },
+  {
+    "revision": "7f71e6275c384b3298a5",
+    "url": "/static/js/2.3e5e7d94.chunk.js"
+  },
+  {
+    "revision": "3459fb29e4b6f567d989663e45bdda91",
+    "url": "/index.html"
+  }
+];
